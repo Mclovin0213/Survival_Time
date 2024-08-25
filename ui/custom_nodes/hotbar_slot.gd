527b8f283719940a2c1caf9b1832c5bba08ae796
Mclovin0213 <javellaneda0213@gmail.com>
2024-08-25 15:15:16 -0700

    small hotbar change

--- ui/custom_nodes/hotbar_slot.gd
+++ ui/custom_nodes/hotbar_slot.gd
@@ -8,4 +8,5 @@ func _can_drop_data(at_position: Vector2, origin_slot: Variant) -> bool:
 	if origin_slot is InventorySlot:
 		return false
 	
+	print(ItemConfig.get_item_resource(origin_slot.item_key).is_equipable)
 	return ItemConfig.get_item_resource(origin_slot.item_key).is_equipable
